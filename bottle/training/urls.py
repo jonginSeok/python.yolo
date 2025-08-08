0faf02a05561c1fe136f6ebdc6c2d087c1a352ec
ngins7512 <ngins7512@gmail.com>
2025-08-08 14:23:16 +0900

    training_input 추가

--- bottle/training/urls.py
+++ bottle/training/urls.py
@@ -5,6 +5,7 @@ app_name = "training"
 
 urlpatterns = [
     path("", views.training_list, name="training_list"),
+    path("input/", views.training_input, name="training_input"),
     path("get_session/<int:session_id>/", views.training_output, name="training_output"),
     # path("api/training/<int:session_id>/", views.training_data_api, name="training_data_api",),
 ]
